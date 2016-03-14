c2c435dd502f58ff511d0d1799d5e6d233ebda4b
Tyler Funk <tfunk@trilixgroup.com>
2016-03-14 16:36:37 -0500

    Updated conf w/ identity and version #
    Also updated pretty.json, so that it contains the updated
    information from the ascendency patch.

--- conf.lua
+++ conf.lua
@@ -1,7 +1,13 @@
 function love.conf(t)
+  t.identity = 'poe-planner-love'
+  t.version = '0.10.1'
+
+  t.window.title = 'PoE Skill Tree Planner'
+  t.window.icon = nil -- @TODO: Provide a window icon
   t.window.width = 1024
   t.window.height = 768
   t.window.resizable = true
+  t.window.highdpi = true
 
-  t.console = true -- Attach a console, windows only?
+  t.console = true -- Attach a console, windows only
 end
