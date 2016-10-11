ebbf6bc072b7cd3e0ba81ba99a1064ae784aa67d
Tyler Funk <tfunk@fastmail.fm>
2016-10-11 13:16:36 -0500

    Logging out graph search levels

--- graph.lua
+++ graph.lua
@@ -226,6 +226,7 @@ end
 
 function searchNearest(currentNodes, level, tiers, visited)
   local tier = {}
+  print(level)
 
   -- Loop through currentNodes, adding neighbors to tier if not visited
   for _,i in ipairs(currentNodes) do
